805ebc50532c6aab61f33e97a4f353dc5c8a2d3f
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-06-28 18:00:58 +0200

    Shadows of the Knight initial commit

---
+++ ShadowsOfTheKnight/src/Main.java
@@ -0,0 +1,27 @@
+
+public class Main {
+	/*
+	 * PROPERTIES
+	 */
+	
+	/* 
+	 * METHODS
+	 */
+
+	
+
+	/*
+	 * GETTERS & SETTERS
+	 */
+	
+	
+	
+	/**
+	 * MAIN
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello world.");
+
+	}
+}
