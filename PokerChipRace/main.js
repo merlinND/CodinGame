439d55ad6a2eba1dbcb29d14cd0707859f753376
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-09-13 19:45:35 +0200

    Basis for a simple solution

--- PokerChipRace/main.js
+++ PokerChipRace/main.js
@@ -5,6 +5,31 @@
  * Tip: merging your chips will give you a sizeable advantage.
  **/
 
+var distance = function(from, to) {
+  return Math.sqrt( Math.pow(from.x - to.x, 2) + Math.pow(from.y - to.y, 2) );
+};
+
+var getMyEntities = function(allEntities) {
+  return allEntities.filter(function(entity) {
+    return (entity.owner == playerId);
+  });
+};
+
+/**
+ * @warning Will return `null` if no entity is eatable
+ */
+var getNearestEatableEntity = function(entities, player) {
+  var eatable = entities.filter(function(entity) {
+    return entity.radius < player.radius;
+  });
+
+  var sorted = eatable.sort(function(a, b) {
+    return distance(a, player) <= distance(b, player);
+  });
+
+  return sorted[0];
+};
+
 // Player identifiers range from 0 to 4
 var playerId = parseInt(readline());
 
@@ -16,32 +41,46 @@ while (true) {
   var entityCount = parseInt(readline());
 
   // ----- Input
+  var entities = [];
   for (var i = 0; i < entityCount; i++) {
     var inputs = readline().split(' ');
+    var entity = {
+      // Unique identifier for this entity
+      id: parseInt(inputs[0]),
+      // The owner of this entity (-1 for neutral droplets)
+      owner: parseInt(inputs[1]),
+      // The current radius of this entity
+      radius: parseFloat(inputs[2]),
 
-    // Unique identifier for this entity
-    var id = parseInt(inputs[0]);
-    // The owner of this entity (-1 for neutral droplets)
-    var player = parseInt(inputs[1]);
-    // The current radius of this entity
-    var radius = parseFloat(inputs[2]);
-
-    // Position (x from 0 to 799, y from 0 to 514)
-    var x = parseFloat(inputs[3]);
-    var y = parseFloat(inputs[4]);
-    // Speed
-    var vx = parseFloat(inputs[5]);
-    var vy = parseFloat(inputs[6]);
+      // Position (x from 0 to 799, y from 0 to 514)
+      x: parseFloat(inputs[3]),
+      y: parseFloat(inputs[4]),
+      // Speed
+      vx: parseFloat(inputs[5]),
+      vy: parseFloat(inputs[6]),
+    };
+    entities.push(entity);
   }
 
   // ----- Game logic
+  var myEntities = getMyEntities(entities);
+
+  printErr("There are", entities.length, "in total (", myEntities.length, " are mine).");
 
   // ----- Output
   // To debug: printErr('Debug messages...');
   // Write an action using print()
-  for (var i = 0; i < playerChipCount; i++) {
+  myEntities.forEach(function(entity) {
     // One instruction per chip: 2 real numbers (x y) for a propulsion, or 'WAIT' to stay still
-    // TODO
-    print('0 0');
-  }
+    // You can append a message to your line, it will get displayed over the entity
+    printErr("Sending instructions for", entity.id);
+
+    var target = getNearestEatableEntity(entities, entity);
+    if(target) {
+      print(target.x, target.y);
+    }
+    else {
+      print('WAIT');
+    }
+  });
 }
