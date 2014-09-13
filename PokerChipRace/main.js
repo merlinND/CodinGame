5341e3355b4f697c737330f2d0542ac4f3f251f9
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-09-13 19:05:11 +0200

    Style

--- PokerChipRace/main.js
+++ PokerChipRace/main.js
@@ -5,27 +5,43 @@
  * Tip: merging your chips will give you a sizeable advantage.
  **/
 
-var playerId = parseInt(readline()); // your id (0 to 4)
+// Player identifiers range from 0 to 4
+var playerId = parseInt(readline());
 
-// game loop
+// Main game loop
 while (true) {
-    var playerChipCount = parseInt(readline()); // The number of chips under your control
-    var entityCount = parseInt(readline()); // The total number of entities on the table, including your chips
-    for (var i = 0; i < entityCount; i++) {
-        var inputs = readline().split(' ');
-        var id = parseInt(inputs[0]); // Unique identifier for this entity
-        var player = parseInt(inputs[1]); // The owner of this entity (-1 for neutral droplets)
-        var radius = parseFloat(inputs[2]); // the radius of this entity
-        var x = parseFloat(inputs[3]); // the X coordinate (0 to 799)
-        var y = parseFloat(inputs[4]); // the Y coordinate (0 to 514)
-        var vx = parseFloat(inputs[5]); // the speed of this entity along the X axis
-        var vy = parseFloat(inputs[6]); // the speed of this entity along the Y axis
-    }
-    for (var i = 0; i < playerChipCount; i++) {
+  // The number of chips under your control
+  var playerChipCount = parseInt(readline());
+  // The total number of entities on the table, including your chips
+  var entityCount = parseInt(readline());
 
-        // Write an action using print()
-        // To debug: printErr('Debug messages...');
+  // ----- Input
+  for (var i = 0; i < entityCount; i++) {
+    var inputs = readline().split(' ');
 
-        print('0 0'); // One instruction per chip: 2 real numbers (x y) for a propulsion, or 'WAIT'.
-    }
+    // Unique identifier for this entity
+    var id = parseInt(inputs[0]);
+    // The owner of this entity (-1 for neutral droplets)
+    var player = parseInt(inputs[1]);
+    // The current radius of this entity
+    var radius = parseFloat(inputs[2]);
+
+    // Position (x from 0 to 799, y from 0 to 514)
+    var x = parseFloat(inputs[3]);
+    var y = parseFloat(inputs[4]);
+    // Speed
+    var vx = parseFloat(inputs[5]);
+    var vy = parseFloat(inputs[6]);
+  }
+
+  // ----- Game logic
+
+  // ----- Output
+  // To debug: printErr('Debug messages...');
+  // Write an action using print()
+  for (var i = 0; i < playerChipCount; i++) {
+    // One instruction per chip: 2 real numbers (x y) for a propulsion, or 'WAIT' to stay still
+    // TODO
+    print('0 0');
+  }
 }
